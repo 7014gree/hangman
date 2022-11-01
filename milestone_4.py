f65037395384261cce5b3a1f004a4920739e3b1c
7014gree <7014gree@gmail.com>
2022-11-01 01:23:57 +0000

    Adds methods for checking valid input and if input is in word

--- milestone_4.py
+++ milestone_4.py
@@ -9,3 +9,22 @@ class Hangman():
         self.word_guessed = ['_' for x in self.word]
         self.num_letters = 0
         self.list_of_guesses = []
+
+    def check_guess(self, guess: str):
+        guess = str.lower(guess)
+        if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
+
+    def ask_for_input(self):
+        while True:
+            guess = input("Guess a letter")
+            if not str.alpha(guess) or len(guess) != 1:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif str.lower(guess) in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+               self.check_guess(guess)
+               self.list_of_guesses.append(guess)
+
+test = Hangman(["apple"])
+test.ask_for_input()
