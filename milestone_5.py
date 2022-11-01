20ee7252af57bed50b5f3e3c7d36c998add2c814
7014gree <7014gree@gmail.com>
2022-11-01 22:57:59 +0000

    Adds play_game function which takes a list of words as input, creates an instance of the Hangman class, and keeps track of the game state by checking whether the game has been won or lost and prompting user input otherwise.

--- milestone_5.py
+++ milestone_5.py
@@ -9,37 +9,52 @@ class Hangman():
         self.num_lives = num_lives
         self.word = random.choice(word_list)
         self.word_guessed = ['_' for x in self.word]
-        self.num_letters = 26
+        self.num_letters = len(set(self.word))
         self.list_of_guesses = []
+        # print(self.word, self.num_letters)
 
     def check_guess(self, guess: str):
         guess = str.lower(guess)
         index = 0
         if guess in self.word:
+            print(f"Good guess! {guess} is in the word.")
             for letter in self.word:
                 if guess == letter:
-                    print(f"Good guess! {guess} is in the word.")
                     self.word_guessed[index] = guess
                 index += 1
+            self.num_letters -= 1
+            print(self.word_guessed, self.num_letters)
         else:
             self.num_lives -= 1
             print(f"Sorry, {guess} is not in the word.")
             print(f"You have {self.num_lives} lives left.")
-        self.num_letters -= 1
         self.list_of_guesses.append(guess)
         
-
-
     def ask_for_input(self):
         while True:
-            guess = input("Guess a letter")
-            if not str.alpha(guess) or len(guess) != 1:
+            guess = input("Guess a letter: ")
+            if not str.isalpha(guess) or len(guess) != 1:
                 print("Invalid letter. Please, enter a single alphabetical character.")
             elif str.lower(guess) in self.list_of_guesses:
                 print("You already tried that letter!")
             else:
                self.check_guess(guess)
                self.list_of_guesses.append(guess)
+            break
 
-test = Hangman(["apple"])
-test.ask_for_input()
+def play_game(word_list: list):
+    game = Hangman(word_list, num_lives = 5)
+
+    while True:
+        if game.num_lives == 0:
+            print("You lost!")
+            break
+        if game.num_letters > 0:
+            game.ask_for_input()
+        if game.num_lives != 0 and game.num_letters < 1:
+            print("Congratulations. You won the game!")
+            break
+
+play_game(["apple", "pear", "orange", "carrot"])
+# %%
+
