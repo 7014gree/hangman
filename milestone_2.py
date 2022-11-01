5cf28690cbd238de9a1f19fd7f8315f3817ed4a1
7014gree <7014gree@gmail.com>
2022-11-01 00:30:25 +0000

    Adds check for valid single character user input

--- milestone_2.py
+++ milestone_2.py
@@ -1,3 +1,4 @@
+from curses.ascii import isalpha
 import random
 
 
@@ -8,3 +9,8 @@ word = random.choice(word_list)
 print(word)
 
 guess = input(f'Enter a single letter')
+
+if str.isalpha(guess) and len(str(guess)) == 1:
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
