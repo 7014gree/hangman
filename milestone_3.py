d920f1e160864c9cf326e2fe8cafe85763ed9986
7014gree <7014gree@gmail.com>
2022-11-01 00:45:44 +0000

    Adds check for whether user input guess is in the word

--- milestone_3.py
+++ milestone_3.py
@@ -6,4 +6,11 @@ while True:
     if str.isalpha(guess) and len(str(guess)) == 1:
         break
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.") 
+        print("Invalid letter. Please, enter a single alphabetical character.")
+
+word = "apple"
+
+if guess in word:
+    print(f"Good guess! {guess} is in the word.")
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
