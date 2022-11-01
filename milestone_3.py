8a85239a3a529ab1d40072d34b777c42a60ca3f7
7014gree <7014gree@gmail.com>
2022-11-01 00:51:59 +0000

    Adds functions for user input and checking guess

--- milestone_3.py
+++ milestone_3.py
@@ -1,16 +1,20 @@
-from curses.ascii import isalpha
+def ask_for_input():
+    while True:
+        guess = input("Guess a letter")
+        if str.isalpha(guess) and len(str(guess)) == 1:
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
 
-
-while True:
-    guess = input("Guess a letter")
-    if str.isalpha(guess) and len(str(guess)) == 1:
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+    check_guess(guess)
 
 word = "apple"
 
-if guess in word:
-    print(f"Good guess! {guess} is in the word.")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+def check_guess(guess: str):
+    guess = str.lower(guess)
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+ask_for_input()
